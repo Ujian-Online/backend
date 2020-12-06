9e7c37e44ba75a4e562c402efa82929ced840cc3
JuniYadi <juniyadi94@gmail.com>
2020-12-06 16:02:38 +0700

    Added Config Options

---
+++ config/options.php
@@ -0,0 +1,22 @@
+<?php
+
+/**
+ * File ini digunakan untuk menyimpan options atau pilihan di database
+ * dan dapat di gunakan kembali pada form dengan menu dropdown atau lainnya
+ *
+ * Data yang dimasukkan berbentuk array
+ */
+
+return [
+    'user_type' => [
+        'admin',
+        'tuk',
+        'assesor',
+        'asessi'
+    ],
+    'user_status' => [
+        'active',
+        'inactive',
+        'suspend'
+    ]
+];
