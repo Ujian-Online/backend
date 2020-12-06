48433ad5a8a8495636c85249d3a76e5ad1378503
JuniYadi <juniyadi94@gmail.com>
2020-12-06 17:53:17 +0700

    Update AuthServiceProvider: Laravel Gate Permission

--- app/Providers/AuthServiceProvider.php
+++ app/Providers/AuthServiceProvider.php
@@ -4,6 +4,7 @@ namespace App\Providers;
 
 use Illuminate\Foundation\Support\Providers\AuthServiceProvider as ServiceProvider;
 use Illuminate\Support\Facades\Gate;
+use Illuminate\Auth\Access\Response;
 
 class AuthServiceProvider extends ServiceProvider
 {
@@ -25,6 +26,40 @@ class AuthServiceProvider extends ServiceProvider
     {
         $this->registerPolicies();
 
-        //
+        // Authentication Checker for Admin
+        Gate::define('isAdmin', function ($user) {
+            return $user->type == 'admin'
+                ? Response::allow()
+                : Response::deny(
+                    trans('permission.gate.error', [':access', 'Administrator'])
+                );
+        });
+
+        // Authentication Checker for TUK
+        Gate::define('isTuk', function ($user) {
+            return $user->type == 'tuk'
+                ? Response::allow()
+                : Response::deny(
+                    trans('permission.gate.error', [':access', 'TUK'])
+                );
+        });
+
+        // Authentication Checker for Assesor
+        Gate::define('isAssesor', function ($user) {
+            return $user->type == 'assesor'
+                ? Response::allow()
+                : Response::deny(
+                    trans('permission.gate.error', [':access', 'Assesor'])
+                );
+        });
+
+        // Authentication Checker for TUK
+        Gate::define('isAsessi', function ($user) {
+            return $user->type == 'asessi'
+                ? Response::allow()
+                : Response::deny(
+                    trans('permission.gate.error', [':access', 'Assesi'])
+                );
+        });
     }
 }
