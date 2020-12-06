72744a03ca09342c91069080c58ab28add8dca56
JuniYadi <juniyadi94@gmail.com>
2020-12-06 16:03:04 +0700

    Added AdminSeeder: Create Account with Admin Access

---
+++ database/seeds/AdminSeeder.php
@@ -0,0 +1,35 @@
+<?php
+
+use App\User;
+use Illuminate\Support\Str;
+use Illuminate\Database\Seeder;
+use Illuminate\Support\Facades\Hash;
+
+class AdminSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run()
+    {
+        // admin user detail
+        $email      = 'admin@test.com';
+        $password   = Str::random(16);
+
+        // create user with admin access
+        User::create([
+            'name' => 'Administrator',
+            'email' => $email,
+            'email_verified_at' => now(),
+            'password' => Hash::make($password),
+            'type' => 'admin',
+            'status' => 'active',
+            'is_active' => true
+        ]);
+
+        // print output login
+        $this->command->info("Create Login Account:\n\nEmail: $email\nPassword: $password");
+    }
+}
