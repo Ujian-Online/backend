55c921a1957db92e4090b2d39c1a08ebf5f76629
JuniYadi <juniyadi94@gmail.com>
2020-12-06 16:02:48 +0700

    Update User Model and Migration

--- database/migrations/2014_10_12_000000_create_users_table.php
+++ database/migrations/2014_10_12_000000_create_users_table.php
@@ -19,6 +19,10 @@ class CreateUsersTable extends Migration
             $table->string('email')->unique();
             $table->timestamp('email_verified_at')->nullable();
             $table->string('password');
+            $table->string('type'); // config('options.user_type')
+            $table->string('status'); // config('options.user_status')
+            $table->string('media_url')->nullable();
+            $table->boolean('is_active');
             $table->rememberToken();
             $table->timestamps();
         });
