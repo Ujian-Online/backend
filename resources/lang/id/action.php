ef6bed59b476fbc8ec61ae541654ddc6a52fd0cb
JuniYadi <juniyadi94@gmail.com>
2020-12-09 15:24:28 +0700

    Added Lang ID: Action and Alert

---
+++ resources/lang/id/action.php
@@ -0,0 +1,13 @@
+<?php
+
+/**
+ * Translate Page untuk Setiap Action Seperti:
+ * - create
+ * - update
+ * - delete
+ */
+
+return [
+    'success'   => 'Berhasil menambahkan data :name ke siste.!',
+    'error'     => 'Gagal menambahkan data :name ke sistem.!'
+];
---
+++ resources/lang/id/alert.php
@@ -0,0 +1,13 @@
+<?php
+
+/**
+ * Translation Page untuk informasi alert
+ */
+
+return [
+    'success'   => 'Sukses',
+    'error'     => 'Gagal',
+    'info'      => 'Info',
+    'important' => 'Penting',
+    'warning'   => 'Peringatan.!'
+];
