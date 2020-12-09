d7fedcb7f4f0c12bdaf76564e13c45cd40223453
JuniYadi <juniyadi94@gmail.com>
2020-12-09 15:23:16 +0700

    Update Layouts: Alert With Close Button

--- resources/views/layouts/alert.blade.php
+++ resources/views/layouts/alert.blade.php
@@ -1,7 +1,17 @@
 @if(session('success'))
-    <div class="alert alert-success">{{ session('success') }}</div>
+    <div class="alert alert-success alert-dismissible fade show" role="alert">
+        <strong>{{ trans('alert.success') }}</strong> {{ session('success') }}
+        <button type="button" class="close" data-dismiss="alert" aria-label="Close">
+            <span aria-hidden="true">&times;</span>
+        </button>
+    </div>
 @endif
 
 @if(session('error'))
-    <div class="alert alert-danger">{{ session('error') }}</div>
+    <div class="alert alert-danger alert-dismissible fade show" role="alert">
+        <strong>{{ trans('alert.error') }}</strong> {{ session('error') }}
+        <button type="button" class="close" data-dismiss="alert" aria-label="Close">
+            <span aria-hidden="true">&times;</span>
+        </button>
+    </div>
 @endif
