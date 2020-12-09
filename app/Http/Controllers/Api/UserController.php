64c7c9419fa27e02de97531cbe9a26aaa6abcb11
JuniYadi <juniyadi94@gmail.com>
2020-12-10 02:20:29 +0700

    Update Controller Api: User - Swagger Docs and Login Function

--- app/Http/Controllers/Api/UserController.php
+++ app/Http/Controllers/Api/UserController.php
@@ -7,15 +7,59 @@ use Illuminate\Http\Request;
 use App\Http\Controllers\Controller;
 use Illuminate\Support\Facades\Auth;
 use Illuminate\Support\Facades\Validator;
+use OpenApi\Annotations as OA;
 
 class UserController extends Controller
 {
-    public function login()
+    /**
+     * @OA\Post(
+     *     path="/api/login",
+     *     tags={"Authentication"},
+     *     summary="Sign In",
+     *
+     *     @OA\RequestBody(
+     *         required=true,
+     *         description="Login Credential",
+     *         @OA\MediaType(
+     *             mediaType="application/json",
+     *             @OA\Schema(
+     *                 @OA\Property(
+     *                     property="email",
+     *                     type="string",
+     *                     format="email",
+     *                     example="test@example.com"
+     *                 ),
+     *                 @OA\Property(
+     *                     property="password",
+     *                     type="string",
+     *                     format="password",
+     *                     example="pass1234"
+     *                 ),
+     *             ),
+     *         ),
+     *     ),
+     *     @OA\Response(
+     *         response="200",
+     *         description="OK"
+     *     )
+     * )
+     */
+    public function login(Request $request)
     {
-        if (Auth::attempt(['email' => request('email'), 'password' => request('password')])) {
-            $user = Auth::user();
+        // validate input request
+        $request->validate([
+            'email'     => 'required|email',
+            'password'  => 'required|min:3|max:255'
+        ]);
 
-            $success['token'] =  $user->createToken('nApp')->accessToken;
+        // get input request
+        $email = $request->input('email');
+        $password = $request->input('password');
+
+        if (Auth::attempt(['email' => $email, 'password' => $password])) {
+            $user = User::findOrFail(Auth::user()->id);
+
+            $success['token'] =  $user->createToken('Api')->accessToken;
 
             return response()->json(['success' => $success], 200);
         } else {
@@ -25,26 +69,36 @@ class UserController extends Controller
 
     public function register(Request $request)
     {
-        $validator = Validator::make($request->all(), [
+        $request->validate([
             'name' => 'required',
             'email' => 'required|email',
             'password' => 'required',
             'c_password' => 'required|same:password',
         ]);
 
-        if ($validator->fails()) {
-            return response()->json(['error'=>$validator->errors()], 401);
-        }
-
         $input = $request->all();
         $input['password'] = bcrypt($input['password']);
         $user = User::create($input);
-        $success['token'] =  $user->createToken('nApp')->accessToken;
+        $success['token'] =  $user->createToken('Api')->accessToken;
         $success['name'] =  $user->name;
 
         return response()->json(['success'=>$success], 200);
     }
 
+
+    /**
+     * @OA\Get(
+     *   path="/api/me",
+     *   tags={"Authentication"},
+     *   summary="Show User Detail",
+     *   security={{"passport":{}}},
+     *
+     *   @OA\Response(
+     *      response="200",
+     *      description="OK"
+     *   )
+     * )
+     */
     public function me(Request $request)
     {
         $user = $request->user();
