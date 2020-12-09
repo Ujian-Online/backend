19f2ac64f71803fa3e3c64f6f077d05dfe7ab4a6
JuniYadi <juniyadi94@gmail.com>
2020-12-09 12:07:19 +0700

    Added DataTables Admin: User DataTables

---
+++ app/DataTables/Admin/UserDataTable.php
@@ -0,0 +1,108 @@
+<?php
+
+namespace App\DataTables\Admin;
+
+use App\User;
+use Yajra\DataTables\Html\Button;
+use Yajra\DataTables\Html\Column;
+use Yajra\DataTables\Html\Editor\Editor;
+use Yajra\DataTables\Html\Editor\Fields;
+use Yajra\DataTables\Services\DataTable;
+
+class UserDataTable extends DataTable
+{
+    /**
+     * Build DataTable class.
+     *
+     * @param mixed $query Results from query() method.
+     * @return \Yajra\DataTables\DataTableAbstract
+     */
+    public function dataTable($query)
+    {
+        return datatables()
+            ->eloquent($query)
+            ->addColumn('action', function ($query) {
+                return view('layouts.pageTableAction', [
+                    'title' => $query->name,
+                    'url_show' => route('admin.user.show', $query->id),
+                    'url_edit' => route('admin.user.edit', $query->id),
+                    'url_destroy' => route('admin.user.destroy', $query->id),
+                ]);
+            });
+    }
+
+    /**
+     * Get query source of dataTable.
+     *
+     * @param \App\User $model
+     * @return \Illuminate\Database\Eloquent\Builder
+     */
+    public function query(User $model)
+    {
+        return $model->newQuery();
+    }
+
+    /**
+     * Optional method if you want to use html builder.
+     *
+     * @return \Yajra\DataTables\Html\Builder
+     */
+    public function html()
+    {
+        return $this->builder()
+                    ->parameters([
+                        'responsive' => true,
+                        'autoWidth' => false,
+                        'lengthMenu' => [
+                            [10, 25, 50, -1],
+                            ['10 rows', '25 rows', '50 rows', 'Show all']
+                        ],
+                        'buttons' => [
+                            'pageLength'
+                        ],
+                    ])
+                    ->setTableId('user-table')
+                    ->columns($this->getColumns())
+                    ->minifiedAjax()
+                    ->dom('Bfrtip')
+                    ->orderBy(5, 'desc')
+                    ->buttons(
+                        Button::make('export'),
+                        Button::make('print'),
+                        Button::make('reset'),
+                        Button::make('reload')
+                    );
+    }
+
+    /**
+     * Get columns.
+     *
+     * @return array
+     */
+    protected function getColumns()
+    {
+        return [
+            Column::make('name'),
+            Column::make('email'),
+            Column::make('type'),
+            Column::make('status'),
+            Column::make('updated_at'),
+            Column::computed('action')
+                ->orderable(false)
+                ->exportable(false)
+                ->printable(false)
+                ->width('15%')
+                ->addClass('text-center'),
+        ];
+    }
+
+    /**
+     * Get filename for export.
+     *
+     * @return string
+     */
+    protected function filename()
+    {
+        return 'User_' . date('YmdHis');
+    }
+}
