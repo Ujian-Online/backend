1e32f662a7a3e98682ca250ad5e869d2fdb29587
JuniYadi <juniyadi94@gmail.com>
2020-12-10 00:30:47 +0700

    Update Route API: Login and Authenticated

--- routes/api.php
+++ routes/api.php
@@ -1,6 +1,7 @@
 <?php
 
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Route;
 
 /*
 |--------------------------------------------------------------------------
@@ -16,3 +17,14 @@ use Illuminate\Http\Request;
 Route::middleware('auth:api')->get('/user', function (Request $request) {
     return $request->user();
 });
+
+Route::namespace('Api')
+    ->name('api.')
+    ->group(function () {
+        Route::post('login', 'UserController@login')->name('login');
+
+        Route::middleware('auth:api')
+            ->group(function () {
+                Route::get('me', 'UserController@me')->name('me');
+            });
+    });
