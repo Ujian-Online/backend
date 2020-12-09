713a54826a00e31deefb2253798899d9c2f99dc6
JuniYadi <juniyadi94@gmail.com>
2020-12-10 02:20:12 +0700

    Update Controller: Swagger Init

--- app/Http/Controllers/Controller.php
+++ app/Http/Controllers/Controller.php
@@ -7,6 +7,17 @@ use Illuminate\Foundation\Bus\DispatchesJobs;
 use Illuminate\Foundation\Validation\ValidatesRequests;
 use Illuminate\Routing\Controller as BaseController;
 
+/**
+ *  @OA\Info(
+ *      title="API Documentation",
+ *      version="0.1"
+ *  )
+ *
+ *  @OA\Server(
+ *      url=L5_SWAGGER_CONST_HOST,
+ *      description="Production API Server"
+ *  )
+ */
 class Controller extends BaseController
 {
     use AuthorizesRequests, DispatchesJobs, ValidatesRequests;
