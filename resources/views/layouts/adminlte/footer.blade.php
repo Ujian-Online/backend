9e5666ffa6b46b2e609a23962462a512f9606795
JuniYadi <juniyadi94@gmail.com>
2020-12-11 17:42:43 +0700

    issue-17: Update Footer CopyRight

--- resources/views/layouts/adminlte/footer.blade.php
+++ resources/views/layouts/adminlte/footer.blade.php
@@ -1,7 +1,10 @@
 <footer class="main-footer">
-    <div class="float-right d-none d-sm-block">
-      <b>Version</b> 3.0.5
-    </div>
-    <strong>Copyright &copy; 2014-2019 <a href="http://adminlte.io">AdminLTE.io</a>.</strong> All rights
-    reserved.
+    {{-- <div class="float-right d-none d-sm-block">
+      <b>Version</b> 0.1
+    </div> --}}
+    <strong>
+      Copyright &copy; {{ \Carbon\Carbon::now()->format('Y') }} 
+      <a href="{{ config('app.url') }}">{{ config('app.name', 'Laravel') }}</a>.
+    </strong>
+    All rights reserved.
 </footer>
