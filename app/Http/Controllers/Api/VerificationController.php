ff1acc8a7e76f40183232acdc1c008d1e648d9f5
JuniYadi <juniyadi94@gmail.com>
2020-12-13 18:42:07 +0700

    Added Controller APi: Vericiation - Verify and Resend Email Verification

---
+++ app/Http/Controllers/Api/VerificationController.php
@@ -0,0 +1,158 @@
+<?php
+
+namespace App\Http\Controllers\Api;
+
+use App\User;
+use Exception;
+use Carbon\Carbon;
+use Illuminate\Http\Request;
+use App\Mail\EmailVerification;
+use App\Http\Controllers\Controller;
+use Illuminate\Auth\Events\Verified;
+use Illuminate\Support\Facades\Mail;
+use Illuminate\Auth\Access\AuthorizationException;
+
+class VerificationController extends Controller
+{
+    /**
+     * Verifikasi Email User
+     *
+     * @param  \Illuminate\Http\Request $request
+     * @param String $hash Hash dari Email
+     *
+     * @return \Illuminate\Http\JsonResponse
+     *
+     *
+     * @OA\Post(
+     *     path="/api/email/verify",
+     *     tags={"Authentication"},
+     *     summary="Email Verification",
+     *     security={{"passport":{}}},
+     *
+     *     @OA\RequestBody(
+     *         required=true,
+     *         @OA\MediaType(
+     *             mediaType="application/json",
+     *             @OA\Schema(
+     *                 @OA\Property(
+     *                     property="hash",
+     *                     type="string",
+     *                     example="eyJ0eXAiOiJKV1QiLCJhbGciOiJSUzI"
+     *                 ),
+     *             ),
+     *         ),
+     *     ),
+     *     @OA\Response(
+     *         response="200",
+     *         description="OK"
+     *     )
+     * )
+     */
+    public function verify(Request $request)
+    {
+        // validate form input
+        $request->validate([
+            'hash' => 'required',
+        ]);
+
+        // get data form input
+        $getInput = $request->only(['hash']);
+
+        try {
+            // check if email has been verified or not
+            if ($request->user()->hasVerifiedEmail()) {
+                throw new Exception('Email sudah di verifikasi.');
+            }
+
+            // parse hash
+            $decryptHash = decrypt($getInput['hash']);
+            $dateHash = Carbon::parse($decryptHash)->timestamp;
+
+            // get date now in timestamp
+            $dateNow = Carbon::now()->timestamp;
+
+            // validate and throw error if expired
+            if ($dateNow > $dateHash) {
+                throw new Exception('Hash tidak ditemukan.!');
+            }
+
+            // get user login
+            $user = $request->user();
+
+            // mark user verified
+            $user->markEmailAsVerified();
+
+            // set status to active
+            $user->status = 'active';
+            $user->update();
+
+            // return error validate
+            return response()->json([
+                'code'      => 200,
+                'success'   => true,
+            ], 200);
+        } catch (Exception $e) {
+            // return error validate
+            return response()->json([
+                'code'      => 400,
+                'success'   => false,
+                'error'     => $e->getMessage()
+            ], 400);
+        }
+    }
+
+    /**
+     * Kirim Ulang Verifikasi Email User
+     *
+     * @param  \Illuminate\Http\Request $request
+     *
+     * @return \Illuminate\Http\JsonResponse
+     *
+     * @OA\Post(
+     *     path="/api/email/resend",
+     *     tags={"Authentication"},
+     *     summary="Resend Email Verification",
+     *     security={{"passport":{}}},
+     *
+     *     @OA\Response(
+     *         response="200",
+     *         description="OK"
+     *     )
+     * )
+     */
+    public function resend(Request $request)
+    {
+        try {
+            // get user login
+            $user = $request->user();
+
+            // check if email has been verified or not
+            if ($user->hasVerifiedEmail()) {
+                throw new Exception('Email sudah di verifikasi.');
+            }
+
+            // Generate User Token
+            $token = $user->createToken('Api')->accessToken;
+
+            // Check if user is verified or not
+            // If not, send email verification
+            if (!$user->hasVerifiedEmail()) {
+                // Send Email Verification to User
+                Mail::to($user->email)->queue(new EmailVerification($token, $user));
+            }
+
+            // return error validate
+            return response()->json([
+                'code'      => 200,
+                'success'   => true,
+            ], 200);
+        } catch (Exception $e) {
+            // return error validate
+            return response()->json([
+                'code'      => 400,
+                'success'   => false,
+                'error'     => $e->getMessage()
+            ], 400);
+        }
+    }
+}
