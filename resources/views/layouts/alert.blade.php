64b811f771c01d91f5b49aae7cdcb2f655be41c8
JuniYadi <juniyadi94@gmail.com>
2020-12-13 14:04:22 +0700

    Update View Layouts: Alert

--- resources/views/layouts/alert.blade.php
+++ resources/views/layouts/alert.blade.php
@@ -1,6 +1,6 @@
 @if(session('success'))
     <div class="alert alert-success alert-dismissible fade show" role="alert">
-        <strong>{{ trans('alert.success') }}</strong> {{ session('success') }}
+        <strong>{{ trans('alert.success') }}</strong>: {{ session('success') }}
         <button type="button" class="close" data-dismiss="alert" aria-label="Close">
             <span aria-hidden="true">&times;</span>
         </button>
@@ -9,7 +9,7 @@
 
 @if(session('error'))
     <div class="alert alert-danger alert-dismissible fade show" role="alert">
-        <strong>{{ trans('alert.error') }}</strong> {{ session('error') }}
+        <strong>{{ trans('alert.error') }}</strong>: {{ session('error') }}
         <button type="button" class="close" data-dismiss="alert" aria-label="Close">
             <span aria-hidden="true">&times;</span>
         </button>
