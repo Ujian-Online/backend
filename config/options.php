4077ee0350c954764be8cb0fbe4dfb36e91a792d
JuniYadi <juniyadi94@gmail.com>
2020-12-14 20:10:15 +0700

    issue-24: Added Option Table User Assesi

--- config/options.php
+++ config/options.php
@@ -18,5 +18,25 @@ return [
         'active',
         'inactive',
         'suspend'
+    ],
+    // true or false
+    'user_assesi_gender' => [
+        0 => 'Wanita',
+        1 => 'Pria',
+    ],
+    'user_assesi_pendidikan_terakhir' => [
+        'SMA',
+        'SMK',
+        'D1',
+        'D2',
+        'D3',
+        'D4',
+        'S1',
+        'S2',
+        'S3',
+    ],
+    'user_assesi_has_job' => [
+        0 => 'Tidak Bekerja',
+        1 => 'Bekerja',
     ]
 ];
