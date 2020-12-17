bc3da09c83366e564f7ea87f2ad757edab8c4431
JuniYadi <juniyadi94@gmail.com>
2020-12-17 14:55:43 +0700

    issue-25: Added DataTables Admin User Assesor

---
+++ app/DataTables/Admin/UserAssesorDataTable.php
@@ -0,0 +1,128 @@
+<?php
+
+namespace App\DataTables\Admin;
+
+use App\UserAssesor;
+use Yajra\DataTables\Html\Button;
+use Yajra\DataTables\Html\Column;
+use Yajra\DataTables\Html\Editor\Editor;
+use Yajra\DataTables\Html\Editor\Fields;
+use Yajra\DataTables\Services\DataTable;
+
+class UserAssesorDataTable extends DataTable
+{
+    /**
+     * Build DataTable class.
+     *
+     * @param mixed $query Results from query() method.
+     * @return \Yajra\DataTables\DataTableAbstract
+     */
+    public function dataTable($query)
+    {
+        return datatables()
+            ->eloquent($query)
+            ->addColumn('action', function ($query) {
+                return view('layouts.pageTableAction', [
+                    'title' => $query->name,
+                    'url_show' => route('admin.assesor.show', $query->id),
+                    'url_edit' => route('admin.assesor.edit', $query->id),
+                    'url_destroy' => route('admin.assesor.destroy', $query->id),
+                ]);
+            });
+    }
+
+    /**
+     * Get query source of dataTable.
+     *
+     * @param \App\UserAssesor $model
+     * @return \Illuminate\Database\Eloquent\Builder
+     */
+    public function query(UserAssesor $model)
+    {
+        return $model->newQuery();
+    }
+
+    /**
+     * Optional method if you want to use html builder.
+     *
+     * @return \Yajra\DataTables\Html\Builder
+     */
+    public function html()
+    {
+        return $this->builder()
+                    ->parameters([
+                        'responsive' => true,
+                        'autoWidth' => false,
+                        'lengthMenu' => [
+                            [10, 25, 50, -1],
+                            ['10 rows', '25 rows', '50 rows', 'Show all']
+                        ],
+                        'dom' => 'Bfrtip',
+                        'buttons' => [
+                            'pageLength',
+                            [
+                                'text' => '<i class="fas fa-plus-circle"></i> ' . trans('table.create'),
+                                'action' => $this->createButton()
+                            ]
+                        ],
+                    ])
+                    ->setTableId('userassesor-table')
+                    ->columns($this->getColumns())
+                    ->minifiedAjax()
+                    ->dom('Bfrtip')
+                    ->orderBy(1, 'asc')
+                    ->buttons(
+                        Button::make('export'),
+                        Button::make('print'),
+                        Button::make('reload')
+                    );
+    }
+
+    /**
+     * Get columns.
+     *
+     * @return array
+     */
+    protected function getColumns()
+    {
+        return [
+            Column::make('id'),
+            Column::make('name'),
+            Column::make('met'),
+            Column::make('expired_date'),
+            Column::make('updated_at')
+                ->title('Update')
+                ->width('10%'),
+            Column::computed('action')
+                ->orderable(false)
+                ->exportable(false)
+                ->printable(false)
+                ->width('15%')
+                ->addClass('text-center'),
+        ];
+    }
+
+    /**
+     * Get filename for export.
+     *
+     * @return string
+     */
+    protected function filename()
+    {
+        return 'UserAssesor_' . date('YmdHis');
+    }
+
+    /**
+     * Custom Create Button Action
+     */
+    public function createButton()
+    {
+        // Create Route URL
+        $url = route('admin.assesor.create');
+
+        // return function redirect
+        return 'function (e, dt, button, config) {
+            window.location = "'. $url .'";
+        }';
+    }
+}
