d2cfecb71c5a1d304cc9f803b7daf08689a4b3c4
JuniYadi <juniyadi94@gmail.com>
2020-12-22 16:01:32 +0700

    issue-45: Update Admin Login Template and Controller Without Username

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -12,16 +12,14 @@
       <form method="POST" action="{{ route('login') }}">
         @csrf
 
-        @include('layouts.alert')
-
         <div class="form-group row">
-          <label for="username" class="col-md-12 col-sm-12 col-form-label">
+          <label for="email" class="col-md-12 col-sm-12 col-form-label">
             {{ trans('theme.email') }}
           </label>
           <div class="col-md-12 col-sm-12">
-              <input id="username" type="text" class="form-control @error('username') is-invalid @enderror" name="username" value="{{ old('username') }}" required autofocus>
+              <input id="email" type="text" class="form-control @error('email') is-invalid @enderror" name="email" value="{{ old('email') }}" required autofocus>
 
-              @error('username')
+              @error('email')
                   <span class="invalid-feedback" role="alert">
                       <strong>{{ $message }}</strong>
                   </span>
