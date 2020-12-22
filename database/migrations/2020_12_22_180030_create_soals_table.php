6812efd75ed1983bb2201aa5d6b0dbe614bcd39e
JuniYadi <juniyadi94@gmail.com>
2020-12-23 03:27:55 +0700

    issue-3: Added Model and Migration - Soal

---
+++ database/migrations/2020_12_22_180030_create_soals_table.php
@@ -0,0 +1,38 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateSoalsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('soals', function (Blueprint $table) {
+            $table->bigIncrements('id');
+            $table->text('question');
+            $table->string('question_type'); // @see config('options
+            //.soals_question_type')
+            $table->string('answer_essay');
+            $table->string('answer_option'); // @see config('options
+            //.soals_answer_option')
+            $table->string('max_score');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('soals');
+    }
+}
