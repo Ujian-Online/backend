ee0539c89131285e7e03ce0fb47daa1905cb9593
JuniYadi <juniyadi94@gmail.com>
2020-12-27 17:15:34 +0700

    issue-48: Update Sertifikasi Model Relation to SertifikasiTuk

--- app/Sertifikasi.php
+++ app/Sertifikasi.php
@@ -3,6 +3,7 @@
 namespace App;
 
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\BelongsToMany;
 
 class Sertifikasi extends Model
 {
@@ -19,4 +20,14 @@ class Sertifikasi extends Model
         'jenis_sertifikasi',
         'is_active',
     ];
+
+    /**
+     * Relation Table to SertifikasiTuk
+     *
+     * @return BelongsToMany
+     */
+    public function SertifikasiTuk()
+    {
+        return $this->hasMany('App\SertifikasiTuk');
+    }
 }
