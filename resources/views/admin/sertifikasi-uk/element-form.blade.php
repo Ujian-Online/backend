10319c8e12b1d0bf87acfccd3b5fddd9d82a0545
JuniYadi <juniyadi94@gmail.com>
2021-01-02 13:09:31 +0700

    issue-53: Update View Admin SertifikasiUKElement Form

--- resources/views/admin/sertifikasi-uk/element-form.blade.php
+++ resources/views/admin/sertifikasi-uk/element-form.blade.php
@@ -5,68 +5,48 @@
 
     <div class="form-row">
 
-        <div class="form-group col-md-6">
-            <label for="asesi_id">Asesi ID</label>
-            <select class="form-control @error('asesi_id') is-invalid @enderror"
-                    name="asesi_id" id="asesi_id">
+        <div class="form-group col-md-12">
+            <label for="unit_kompetensi_id">Sertifikasi Unit Kompetensi ID</label>
+            <select class="form-control @error('unit_kompetensi_id') is-invalid @enderror"
+                    name="unit_kompetensi_id" id="unit_kompetensi_id">
 
-                @foreach($asesis as $asesi)
+                @foreach($unitkompentensis as $unitkompentensi)
                     <option
-                        value="{{ $asesi->id }}"
-                    @if(!empty(request()->query('asesi_id')) and request()
-                    ->query('asesi_id') == $asesi->id)
-                        {{  __('selected') }}
-                        @elseif(isset($query->asesi_id) and $query->asesi_id ==
-                        $asesi->id)
-                        {{  __('selected') }}
+                        value="{{ $unitkompentensi->id }}"
+
+                        @if(!empty(request()->query('unit_kompetensi_id')) and request()->query('unit_kompetensi_id') == $unitkompentensi->id)
+                            {{  __('selected') }}
+                        @elseif(isset($query->unit_kompetensi_id) and $query->unit_kompetensi_id ==
+                        $unitkompentensi->id)
+                            {{  __('selected') }}
                         @endif
                     >
-                        [ID: {{ $asesi->id }}] - {{ $asesi->name }}
+                        [ID: {{ $unitkompentensi->id }}] - {{ $unitkompentensi->title }} (Kode: {{ $unitkompentensi->kode_unit_kompetensi }})
                     </option>
                 @endforeach
 
             </select>
         </div>
 
-        <div class="form-group col-md-6">
-            <label for="sertifikasi_id">Sertifikasi ID</label>
-            <select class="form-control @error('sertifikasi_id') is-invalid @enderror"
-                    name="sertifikasi_id" id="sertifikasi_id">
 
-                @foreach($sertifikasis as $sertifikasi)
-                    <option
-                        value="{{ $sertifikasi->id }}"
-                    @if(!empty(request()->query('sertifikasi_id')) and request()->query('sertifikasi_id') == $sertifikasi->id)
-                        {{  __('selected') }}
-                        @elseif(isset($query->sertifikasi_id) and $query->sertifikasi_id ==
-                        $sertifikasi->id)
-                        {{  __('selected') }}
-                        @endif
-                    >
-                        [ID: {{ $sertifikasi->id }}] - {{ $sertifikasi->title }} (Nomor Skema: {{ $sertifikasi->nomor_skema }})
-                    </option>
-                @endforeach
+        <div class="form-group col-md-12">
+            <label for="desc">Deskripsi</label>
+            <textarea class="form-control @error('desc') is-invalid @enderror" name="desc" id="desc" cols="30" rows="5" @if(isset($isShow)) readonly @endif>{{ old('desc') ?? $query->desc ?? '' }}</textarea>
 
-            </select>
-        </div>
-
-        <div class="form-group col-md-6">
-            <label for="tuk_price_baru">Harga Baru TUK</label>
-            <input type="number" class="form-control @error('tuk_price_baru') is-invalid @enderror" name="tuk_price_baru" id="tuk_price_baru" placeholder="Harga Baru TUK" value="{{ old('tuk_price_baru') ?? $query->tuk_price_baru ?? '' }}" @if(isset($isShow)) readonly @endif>
-
-            @error('tuk_price_baru')
-            <div class="alert alert-danger">{{ $message }}</div>
+            @error('desc')
+                <div class="alert alert-danger">{{ $message }}</div>
             @enderror
         </div>
 
-        <div class="form-group col-md-6">
-            <label for="tuk_price_perpanjang">Harga Perpanjang TUK</label>
-            <input type="number" class="form-control @error('tuk_price_perpanjang') is-invalid @enderror" name="tuk_price_perpanjang" id="tuk_price_perpanjang" placeholder="Harga Perpanjang TUK" value="{{ old('tuk_price_perpanjang') ?? $query->tuk_price_perpanjang ?? '' }}" @if(isset($isShow)) readonly @endif>
+        <div class="form-group col-md-12">
+            <label for="upload_instruction">Instruksi Upload</label>
+            <textarea class="form-control @error('upload_instruction') is-invalid @enderror" name="upload_instruction" id="upload_instruction" cols="30" rows="5" @if(isset($isShow)) readonly @endif>{{ old('upload_instruction') ?? $query->upload_instruction ?? '' }}</textarea>
 
-            @error('tuk_price_perpanjang')
-            <div class="alert alert-danger">{{ $message }}</div>
+            @error('upload_instruction')
+                <div class="alert alert-danger">{{ $message }}</div>
             @enderror
         </div>
+
     </div>
 @endsection
 
