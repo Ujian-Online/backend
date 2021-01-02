972d6ef752bb954003f49b9250eda4af16c17ca7
JuniYadi <juniyadi94@gmail.com>
2021-01-02 12:44:24 +0700

    issue-52: Update LANG Action

--- resources/lang/id/action.php
+++ resources/lang/id/action.php
@@ -8,8 +8,8 @@
  */
 
 return [
-    'success'           => 'Berhasil menambahkan data :name ke sistem.!',
-    'success_update'    => 'Berhasil mengubah data :name di sistem.!',
+    'success'           => 'Berhasil menambahkan data: :name ke sistem.!',
+    'success_update'    => 'Berhasil mengubah data: :name di sistem.!',
     'error'             => 'Gagal menambahkan data :name ke sistem.!',
     'error_assesor'     => 'User ID: :id telah dipilih oleh Assesor lain.!',
     'save'              => 'Simpan',
