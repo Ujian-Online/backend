277cf9ee5689756048d57a869558ba0b444b537c
JuniYadi <juniyadi94@gmail.com>
2021-01-03 20:07:50 +0700

    issue-62: Update Menu AdminLTE

--- config/adminlte.php
+++ config/adminlte.php
@@ -203,6 +203,12 @@ return [
                 'route'     => 'admin.ujian.jawaban.index',
                 'active'    => 'admin.ujian.jawaban.*',
             ],
+            [
+                'title'     => 'Ujian Asesi Jawaban Pilihan',
+                'icon'      => 'fas fa-list',
+                'route'     => 'admin.ujian.jawabanpilihan.index',
+                'active'    => 'admin.ujian.jawabanpilihan.*',
+            ],
         ],
         'tuk' => [],
         'assesor' => [],
