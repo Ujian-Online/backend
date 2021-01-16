a5e04b0ca52600521ac4f5f41fd59a94be06ff0f
JuniYadi <juniyadi94@gmail.com>
2021-01-16 17:52:31 +0700

    issue-83: Update API OrderController - Show Function

--- app/Http/Controllers/Api/OrderController.php
+++ app/Http/Controllers/Api/OrderController.php
@@ -41,4 +41,46 @@ class OrderController extends Controller
             ->where('users.id', $user->id)
             ->paginate(10);
     }
+
+    /**
+     * @param Request $request
+     * @param         $id
+     *
+     * @return \Illuminate\Database\Eloquent\Builder|\Illuminate\Database\Eloquent\Model|\Illuminate\Database\Query\Builder
+     *
+     * @OA\Get(
+     *   path="/api/order/{id}",
+     *   tags={"Order"},
+     *   summary="Order Detail by ID",
+     *   security={{"passport":{}}},
+     *
+     *   @OA\Parameter(
+     *      name="id",
+     *      description="Order id",
+     *      required=true,
+     *      in="path",
+     *      @OA\Schema(
+     *          type="integer"
+     *      )
+     *   ),
+     *   @OA\Response(
+     *      response="200",
+     *      description="OK"
+     *   )
+     * )
+     */
+    public function show(Request $request, $id)
+    {
+        // get user login
+        $user = $request->user();
+
+        // get order detail by ID
+        return Order::with(['sertifikasi', 'tuk', 'asesi'])
+            ->leftjoin('user_asesis', 'user_asesis.id', '=', 'orders.asesi_id')
+            ->leftjoin('users', 'users.id', '=', 'user_asesis.user_id')
+            ->select('orders.*')
+            ->where('users.id', $user->id)
+            ->where('orders.id', $id)
+            ->firstOrFail();
+    }
 }
