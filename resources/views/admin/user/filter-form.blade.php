631d42aa305d7aeb78e34316736019f227d87131
JuniYadi <juniyadi94@gmail.com>
2021-01-20 23:20:26 +0700

    issue-91: Update View Admin User Filter Form - Show Hide Filter

--- resources/views/admin/user/filter-form.blade.php
+++ resources/views/admin/user/filter-form.blade.php
@@ -1,10 +1,20 @@
 <div class="col-md-12">
-    <div class="card card-outline card-primary">
-        <div class="card-header text-bold">
-            Filter Data
+    <div class="card card-outline card-primary @if(empty(request()->input('filter'))) {{ _('collapsed-card')  }} @endif">
+        <div class="card-header">
+            <h3 class="card-title">Filter Data</h3>
+            <div class="card-tools">
+                <button type="button" class="btn btn-tool" data-card-widget="collapse">
+                    @if(empty(request()->input('filter')))
+                        <i class="fas fa-plus"></i>
+                    @else
+                        <i class="fas fa-minus"></i>
+                    @endif
+                </button>
+            </div>
         </div>
         <div class="card-body">
             <form id="filter-form" action="{{ $filter_route }}" method="GET">
+                <input type="hidden" name="filter" value="true">
                 <div class="form-row">
                     <div class="form-group col-md-6">
                         <label for="type">Type</label>
