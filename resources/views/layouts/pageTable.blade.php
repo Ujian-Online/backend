221d4dbc0b2016f64cec8c54238967d3be903fb1
JuniYadi <juniyadi94@gmail.com>
2021-01-20 22:26:22 +0700

    issue-91: Update View Page Table - Include Filter Form View and Route

--- resources/views/layouts/pageTable.blade.php
+++ resources/views/layouts/pageTable.blade.php
@@ -5,8 +5,14 @@
 @section('content')
 <div class="container">
     <div class="row justify-content-center">
+
+        {{-- Only Include View If Object View Found --}}
+        @if(isset($filter_view) and !empty($filter_view))
+            @include($filter_view)
+        @endif
+
         <div class="col-md-12">
-            <div class="card">
+            <div class="card card-outline card-success">
                 <div class="card-body">
                     @include('layouts.alert')
 
@@ -22,4 +28,10 @@
 <script src="{{ mix('/js/admin.js') }}"></script>
 <script src="{{ asset('vendor/datatables/buttons.server-side.js') }}"></script>
 {!! $dataTable->scripts() !!}
+<script>
+    $('select').select2({
+        theme: 'bootstrap4',
+        disabled: false
+    });
+</script>
 @endsection
