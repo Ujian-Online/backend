e6e662b9e51ee6ac9ccea5cca8d8379faef93293
JuniYadi <juniyadi94@gmail.com>
2021-02-02 00:33:52 +0700

    issue-114: Update Admin Sertifikasi UK View Form - Sertifikasi Using Ajax Request

--- resources/views/admin/sertifikasi-uk/form.blade.php
+++ resources/views/admin/sertifikasi-uk/form.blade.php
@@ -8,22 +8,7 @@
         <div class="form-group col-md-12">
             <label for="sertifikasi_id">Sertifikasi ID</label>
             <select class="form-control @error('sertifikasi_id') is-invalid @enderror"
-                    name="sertifikasi_id" id="sertifikasi_id">
-
-                @foreach($sertifikasis as $sertifikasi)
-                    <option
-                        value="{{ $sertifikasi->id }}"
-                    @if(!empty(request()->query('sertifikasi_id')) and request()->query('sertifikasi_id') == $sertifikasi->id)
-                        {{  __('selected') }}
-                        @elseif(isset($query->sertifikasi_id) and $query->sertifikasi_id ==
-                        $sertifikasi->id)
-                        {{  __('selected') }}
-                        @endif
-                    >
-                        [ID: {{ $sertifikasi->id }}] - {{ $sertifikasi->title }} (Nomor Skema: {{ $sertifikasi->nomor_skema }})
-                    </option>
-                @endforeach
-
+                    name="sertifikasi_id" id="sertifikasi_id" data-placeholder="Pilih Sertifikasi ID">
             </select>
         </div>
 
@@ -98,10 +83,62 @@
 
 @section('js')
     <script>
-        $('select').select2({
+        /**
+         * Select2 with Ajax Start
+         * @type {string}
+         */
+
+            // default selected sertifikasi_id from query URL
+        const sertifikasi_id_default = '{{ request()->input('sertifikasi_id') ?? $query->sertifikasi_id ?? null }}'
+        // trigger load data if sertifikasi_id not null
+        if(sertifikasi_id_default) {
+            var sertifikasiSelected = $('#sertifikasi_id');
+            $.ajax({
+                url: '{{ route('admin.sertifikasi.search') }}' + '?q=' + sertifikasi_id_default,
+                dataType: 'JSON',
+            }).then(function (data) {
+                // create the option and append to Select2
+                var option = new Option(data[0].text, data[0].id, true, true);
+                sertifikasiSelected.append(option).trigger('change');
+
+                // manually trigger the `select2:select` event
+                sertifikasiSelected.trigger({
+                    type: 'select2:select',
+                    params: {
+                        data: data
+                    }
+                });
+            });
+        }
+
+        // sertifikasi select2 with ajax query search
+        $('#sertifikasi_id').select2({
             theme: 'bootstrap4',
-            disabled: {{ (isset($isShow) and !empty($isShow)) ? 'true' : 'false' }}
+            disabled: {{ (!empty(request()->input('sertifikasi_id'))) ?? (isset($isShow) and !empty($isShow)) ? 'true' : 'false' }},
+            allowClear: true,
+            minimumInputLength: 1,
+            ajax: {
+                url: '{{ route('admin.sertifikasi.search') }}',
+                dataType: 'JSON',
+                delay: 100,
+                cache: false,
+                data: function (data) {
+                    return {
+                        q: data.term
+                    }
+                },
+                processResults: function (response) {
+                    return {
+                        results: response
+                    }
+                }
+            },
         });
+
+        /**
+         * Select2 with Ajax End
+         * @type {string}
+         */
     </script>
     <script>
         // clear ukid on load
