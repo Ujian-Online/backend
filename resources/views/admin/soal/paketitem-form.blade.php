f355ef3db90b35c39d3f1160d46267d70ce7ee5b
JuniYadi <juniyadi94@gmail.com>
2021-02-03 00:05:52 +0700

    issue-116: Update View Admin PaketItem Form - Select2 Ajax

--- resources/views/admin/soal/paketitem-form.blade.php
+++ resources/views/admin/soal/paketitem-form.blade.php
@@ -5,25 +5,10 @@
 
     <div class="form-row">
 
-
         <div class="form-group col-md-12">
             <label for="soal_id">Soal ID</label>
-            <select class="form-control @error('soal_id') is-invalid @enderror" name="soal_id" id="soal_id">
-
-                @foreach($soals as $soals)
-                    <option
-                        value="{{ $soals->id }}"
-
-                        @if(!empty(request()->query('soal_id')) and request()->query('soal_id') == $soals->id)
-                            {{  __('selected') }}
-                        @elseif(isset($query->soal_id) and $query->soal_id == $soals->id)
-                            {{  __('selected') }}
-                        @endif
-                    >
-                        [ID: {{ $soals->id }}] - {{ $asesi->question }}
-                    </option>
-                @endforeach
-
+            <select class="form-control @error('soal_id') is-invalid @enderror"
+                    name="soal_id" id="soal_id" data-placeholder="Pilih Soal ID">
             </select>
 
             @error('soal_id')
@@ -33,22 +18,8 @@
 
         <div class="form-group col-md-12">
             <label for="soal_paket_id">Soal Paket ID</label>
-            <select class="form-control @error('soal_paket_id') is-invalid @enderror" name="soal_paket_id" id="soal_paket_id">
-
-                @foreach($soalpakets as $soalpaket)
-                    <option
-                        value="{{ $soalpaket->id }}"
-
-                        @if(!empty(request()->query('soal_paket_id')) and request()->query('soal_paket_id') == $soalpaket->id)
-                            {{  __('selected') }}
-                        @elseif(isset($query->soal_paket_id) and $query->soal_paket_id == $soalpaket->id)
-                            {{  __('selected') }}
-                        @endif
-                    >
-                        [ID: {{ $soalpaket->id }}] - {{ $soalpaket->title }}
-                    </option>
-                @endforeach
-
+            <select class="form-control @error('soal_paket_id') is-invalid @enderror"
+                    name="soal_paket_id" id="soal_paket_id" data-placeholder="Pilih Soal Paket ID">
             </select>
 
             @error('soal_paket_id')
@@ -60,9 +31,119 @@
 
 @section('js')
     <script>
-        $('select').select2({
+        /**
+         * Select2 with Ajax Start
+         * @type {string}
+         */
+
+            // default selected soal_id from query URL
+        const soal_id_default = '{{ request()->input('soal_id') ?? null }}'
+        // trigger load data if soal_id not null
+        if(soal_id_default) {
+            var soalSelected = $('#soal_id');
+            $.ajax({
+                url: '{{ route('admin.soal.search') }}' + '?q=' + soal_id_default,
+                dataType: 'JSON',
+            }).then(function (data) {
+                // create the option and append to Select2
+                var option = new Option(data[0].text, data[0].id, true, true);
+                soalSelected.append(option).trigger('change');
+
+                // manually trigger the `select2:select` event
+                soalSelected.trigger({
+                    type: 'select2:select',
+                    params: {
+                        data: data
+                    }
+                });
+            });
+        }
+
+        // soal select2 with ajax query search
+        $('#soal_id').select2({
             theme: 'bootstrap4',
-            disabled: {{ (isset($isShow) and !empty($isShow)) ? 'true' : 'false' }}
+            disabled: {{ (isset($isShow) and !empty($isShow)) ? 'true' : 'false' }},
+            allowClear: true,
+            minimumInputLength: 1,
+            ajax: {
+                url: '{{ route('admin.soal.search') }}',
+                dataType: 'JSON',
+                delay: 100,
+                cache: false,
+                data: function (data) {
+                    return {
+                        q: data.term
+                    }
+                },
+                processResults: function (response) {
+                    return {
+                        results: response
+                    }
+                }
+            },
         });
+
+        /**
+         * Select2 with Ajax End
+         * @type {string}
+         */
+    </script>
+    <script>
+        /**
+         * Select2 with Ajax Start
+         * @type {string}
+         */
+
+            // default selected soal_paket_id from query URL
+        const soal_paket_id_default = '{{ request()->input('soal_paket_id') ?? null }}'
+        // trigger load data if soal_id not null
+        if(soal_paket_id_default) {
+            var soalPaketSelected = $('#soal_paket_id');
+            $.ajax({
+                url: '{{ route('admin.soal.paket.search') }}' + '?q=' + soal_paket_id_default,
+                dataType: 'JSON',
+            }).then(function (data) {
+                // create the option and append to Select2
+                var option = new Option(data[0].text, data[0].id, true, true);
+                soalPaketSelected.append(option).trigger('change');
+
+                // manually trigger the `select2:select` event
+                soalPaketSelected.trigger({
+                    type: 'select2:select',
+                    params: {
+                        data: data
+                    }
+                });
+            });
+        }
+
+        // soal select2 with ajax query search
+        $('#soal_paket_id').select2({
+            theme: 'bootstrap4',
+            disabled: {{ (isset($isShow) and !empty($isShow)) ? 'true' : 'false' }},
+            allowClear: true,
+            minimumInputLength: 1,
+            ajax: {
+                url: '{{ route('admin.soal.paket.search') }}',
+                dataType: 'JSON',
+                delay: 100,
+                cache: false,
+                data: function (data) {
+                    return {
+                        q: data.term
+                    }
+                },
+                processResults: function (response) {
+                    return {
+                        results: response
+                    }
+                }
+            },
+        });
+
+        /**
+         * Select2 with Ajax End
+         * @type {string}
+         */
     </script>
 @endsection
