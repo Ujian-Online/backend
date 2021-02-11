7730a2d222dfaa92729573ca84c9f23107768b6b
JuniYadi <juniyadi94@gmail.com>
2021-02-11 22:46:40 +0700

    issue-138: Update API Apl01Controller - APL01 Custom Data

--- app/Http/Controllers/Api/Apl01Controller.php
+++ app/Http/Controllers/Api/Apl01Controller.php
@@ -5,6 +5,7 @@ namespace App\Http\Controllers\Api;
 use App\AsesiCustomData;
 use App\Http\Controllers\Controller;
 use App\UserAsesi;
+use App\UserAsesiCustomData;
 use Illuminate\Http\Request;
 
 class Apl01Controller extends Controller
@@ -66,7 +67,7 @@ class Apl01Controller extends Controller
      *
      *     @OA\RequestBody(
      *         required=true,
-     *         description="Update Order Data",
+     *         description="Update APL01 Data",
      *         @OA\MediaType(
      *             mediaType="application/json",
      *             @OA\Schema(
@@ -206,7 +207,7 @@ class Apl01Controller extends Controller
         $dataInput['user_id'] = $user->id;
 
         // check if data found or not
-        $queryData = UserAsesi::where('user_id', $user->id)->firstOrFail();
+        $queryData = UserAsesi::where('user_id', $user->id)->first();
 
         // kalau data ada, maka update
         if($queryData) {
@@ -221,5 +222,110 @@ class Apl01Controller extends Controller
 
         // return json response
         return response()->json($queryData);
+    }
+
+    /**
+     * @param Request $request
+     * @return \Illuminate\Http\JsonResponse
+     *
+     * @OA\Post(
+     *   path="/api/apl01/customdata",
+     *   tags={"APL01"},
+     *   summary="Update APL01 Custom Data",
+     *   security={{"passport":{}}},
+     *
+     *     @OA\RequestBody(
+     *         required=true,
+     *         description="Update Custom Data",
+     *         @OA\MediaType(
+     *             mediaType="multipart/form-data",
+     *             @OA\Schema(
+     *                 @OA\Property(
+     *                     property="customdataid",
+     *                     description="Custom Data ID dari Index Custom Data dari API: GET /api/apl01 object customdata",
+     *                     type="integer",
+     *                     example="1"
+     *                 ),
+     *                 @OA\Property(
+     *                     property="value",
+     *                     description="Value ini berbeda-beda inputannya,
+     *                         tergantung dari custom data, jika file upload, maka berupa file ekstensi: JPG/JPEG/PNG/PDF,
+     *                          jika dropdown maka sesuai value dari dropdown_option, jika text, maka text biasa.",
+     *                     type="file",
+     *                     example="scan_ijazah.pdf"
+     *                 ),
+     *             ),
+     *         ),
+     *     ),
+     *     @OA\Response(
+     *         response="200",
+     *         description="OK"
+     *     )
+     * )
+     */
+    public function customdata(Request $request)
+    {
+        // get user login
+        $user = $request->user();
+
+        // before save custom data, asesi need fill all apl01 first
+        $apl01 = UserAsesi::where('user_id', $user->id)->first();
+
+        // return error klo APL01 blm lengkap
+        if(!$apl01) {
+            return response()->json([
+                'message' => 'Gagal update custom data, karena APL-01 belum lengkap.!'
+            ], 403);
+        }
+
+        // get custom data id before validate
+        $customdataid = $request->input('customdataid');
+        // check index custom data
+        $customdata = AsesiCustomData::findOrFail($customdataid);
+
+        // variable for save value
+        $value = $request->input('value');
+
+        // request validate based on input type
+        if($customdata->input_type == 'upload_image' OR $customdata->input_type == 'upload_pdf') {
+            $request->validate([
+                'customdataid'  => 'required',
+                'value'         => 'required|mimes:jpg,jpeg,png,pdf'
+            ]);
+
+            // run upload data
+            $value = upload_to_s3($request->file('value'));
+        } else if($customdata->input_type == 'dropdown') {
+            $request->validate([
+                'customdataid'  => 'required',
+                'value'         => 'required|in:' . $customdata->dropdown_option
+            ]);
+        } else {
+            $request->validate([
+                'customdataid'  => 'required',
+                'value'         => 'required'
+            ]);
+        }
+
+        // check asesi apl01 custom data, found or not, for update or create new element
+        $apl01customdata = UserAsesiCustomData::where('asesi_id', $user->id)
+            ->where('input_type', $customdata->input_type)->first();
+
+        // update jika ada customdata
+        if($apl01customdata) {
+            $apl01customdata->value = $value;
+            $save = $apl01customdata->save();
+        } else {
+            // buat baru jika tidak ada customdata
+            $save = UserAsesiCustomData::create([
+                'asesi_id' => $user->id,
+                'title' => $customdata->title,
+                'input_type' => $customdata->input_type,
+                'value' => $value,
+                'is_verified' => false,
+            ]);
+        }
+
+        return response()->json($save);
     }
 }
