84c344840d07efb887b1d275ca5c83e563b90f96
JuniYadi <juniyadi94@gmail.com>
2021-02-11 22:46:54 +0700

    issue-138: Update Route API Custom Data

--- routes/api.php
+++ routes/api.php
@@ -44,5 +44,6 @@ Route::name('api.')
 
                 Route::get('apl01', 'Api\Apl01Controller@index')->name('apl01.index');
                 Route::post('apl01', 'Api\Apl01Controller@store')->name('apl01.store');
+                Route::post('apl01/customdata', 'Api\Apl01Controller@customdata')->name('apl01.store');
             });
     });
