d0a4fc7b5ff1ff000f8d15dab202bee21c24f05a
JuniYadi <juniyadi94@gmail.com>
2021-02-21 06:23:24 +0700

    issue-159: Added Tuk UjianAsesiAsesorDataTable with Filter by TUK

---
+++ app/DataTables/Tuk/UjianAsesiAsesorDataTable.php
@@ -0,0 +1,189 @@
+<?php
+
+namespace App\DataTables\Tuk;
+
+use App\UjianAsesiAsesor;
+use Yajra\DataTables\Html\Button;
+use Yajra\DataTables\Html\Column;
+use Yajra\DataTables\Html\Editor\Editor;
+use Yajra\DataTables\Html\Editor\Fields;
+use Yajra\DataTables\Services\DataTable;
+
+class UjianAsesiAsesorDataTable extends DataTable
+{
+    /**
+     * Build DataTable class.
+     *
+     * @param mixed $query Results from query() method.
+     * @return \Yajra\DataTables\DataTableAbstract
+     */
+    public function dataTable($query)
+    {
+        return datatables()
+            ->eloquent($query)
+            ->addColumn('name_asesi', function($query) {
+                $name_asesi = $query->userasesi->email;
+
+                if(isset($query->userasesi->asesi) and !empty($query->userasesi->asesi) and isset($query->userasesi->asesi->name) and !empty($query->userasesi->asesi->name)) {
+                    $name_asesi = $query->userasesi->asesi->name;
+                }
+
+                return $name_asesi;
+            })
+            ->addColumn('name_asesor', function($query) {
+                $name_asesor = $query->userasesor->email;
+
+                if(isset($query->userasesor->asesor) and !empty($query->userasesor->asesor) and isset($query->userasesor->asesor->name) and !empty($query->userasesor->asesor->name)) {
+                    $name_asesor = $query->userasesor->asesor->name;
+                }
+
+                return $name_asesor;
+            })
+            ->editColumn('status', function($query) {
+                return $query->status ? ucwords(str_replace('_', ' ', $query->status)) : '';
+            })
+            ->editColumn('is_kompeten', function ($query) {
+                $kompeten = '';
+                // cek apa datanya ada atau tidak
+                if(!empty($query->is_kompeten)) {
+                    $kompeten = $query->is_kompeten ? 'Kompeten' : 'Tidak Kompeten';
+                }
+                // return kompeten status
+                return $kompeten;
+            });
+    }
+
+    /**
+     * Get query source of dataTable.
+     *
+     * @param \App\UjianAsesiAsesor $model
+     * @return \Illuminate\Database\Eloquent\Builder
+     */
+    public function query(UjianAsesiAsesor $model)
+    {
+        // default query
+        $query = $model->with([
+            'userasesi',
+            'userasesi.asesi',
+            'userasesor',
+            'userasesor.asesor',
+            'ujianjadwal',
+            'sertifikasi',
+            'order'
+        ]);
+
+        // get input filter
+        $ujian_jadwal_id = request()->input('ujian_jadwal_id');
+        $sertifikasi_id = request()->input('sertifikasi_id');
+        $status = request()->input('status');
+
+        // filter by jadwal id
+        if(!empty($ujian_jadwal_id)) {
+            $query = $query->whereHas('ujianjadwal', function($query) use ($ujian_jadwal_id) {
+                $query->where('id', $ujian_jadwal_id);
+            });
+        }
+
+        // filter by sertifikasi
+        if(!empty($sertifikasi_id)) {
+            $query = $query->whereHas('sertifikasi', function($query) use ($sertifikasi_id) {
+                $query->where('id', $sertifikasi_id);
+            });
+        }
+
+        // filter by ujian jadwal
+        if(!empty($status)) {
+            $query = $query->where('status', $status);
+        }
+
+        // get user login
+        $user = request()->user();
+        // change index if loggin by tuk
+        if($user->can('isTuk')) {
+            // get tuk id
+            $tuk = $user->tuk->tuk_id;
+
+            // filter tuk id based on order data
+            $query = $query->whereHas('order', function($query) use ($tuk) {
+                $query->where('tuk_id', $tuk);
+            });
+        }
+
+        return $query;
+    }
+
+    /**
+     * Optional method if you want to use html builder.
+     *
+     * @return \Yajra\DataTables\Html\Builder
+     */
+    public function html()
+    {
+        return $this->builder()
+                    ->parameters([
+                        'responsive' => true,
+                        'autoWidth' => false,
+                        'lengthMenu' => [
+                            [10, 25, 50, -1],
+                            ['10 rows', '25 rows', '50 rows', 'Show all']
+                        ],
+                        'dom' => 'Bfrtip',
+                        'buttons' => [
+                            'pageLength'
+                        ],
+                    ])
+                    ->setTableId('ujianasesiasesor-table')
+                    ->columns($this->getColumns())
+                    ->minifiedAjax()
+                    ->dom('Bfrtip')
+                    ->orderBy(1)
+                    ->buttons(
+                        // Button::make('export'),
+                        // Button::make('print'),
+                        Button::make('reload')
+                    );
+    }
+
+    /**
+     * Get columns.
+     *
+     * @return array
+     */
+    protected function getColumns()
+    {
+        return [
+            Column::computed('jadwal')
+                ->title('Jadwal Ujian')
+                ->data('ujianjadwal.title')
+                ->width('10%'),
+            Column::computed('tanggal')
+                ->title('Tgl Ujian')
+                ->data('ujianjadwal.tanggal')
+                ->width('10%'),
+            Column::computed('name_asesi')
+                ->title('Asesi')
+                ->width('10%'),
+            Column::computed('name_asesor')
+                ->title('Asesor')
+                ->width('10%'),
+            Column::computed('sertifikasi')
+                ->title('Sertifikasi')
+                ->data('sertifikasi.title')
+                ->width('20%'),
+            Column::make('status')
+                ->width('10%'),
+            Column::make('is_kompeten')
+                ->width('5%'),
+        ];
+    }
+
+    /**
+     * Get filename for export.
+     *
+     * @return string
+     */
+    protected function filename()
+    {
+        return 'UjianAsesiAsesor_' . date('YmdHis');
+    }
+}
