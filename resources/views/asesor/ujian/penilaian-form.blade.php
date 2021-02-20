e9558024c6a9a27d4a986c4534158187bfcb64da
JuniYadi <juniyadi94@gmail.com>
2021-02-20 21:26:20 +0700

    Update View Asesor Ujian PenilaianForm - Javascript Sum and Calculation with new table view

--- resources/views/asesor/ujian/penilaian-form.blade.php
+++ resources/views/asesor/ujian/penilaian-form.blade.php
@@ -131,9 +131,7 @@
                             <thead>
                             <tr class="text-center">
                                 <th width="5%" style="vertical-align: middle;">ID</th>
-                                <th width="50%" style="vertical-align: middle;">Pertanyaan</th>
-                                <th width="20%" style="vertical-align: middle;">Jawaban Asesi</th>
-                                <th width="20%" style="vertical-align: middle;">Jawaban Benar</th>
+                                <th width="90%" style="vertical-align: middle;">Pertanyaan</th>
                                 <th width="5%" style="vertical-align: middle;">Score</th>
                             </tr>
                             </thead>
@@ -142,6 +140,8 @@
                                 @foreach($soal_pilihangandas as $soal_pilihanganda)
                                     <tr id="pilihanganda-{{ $soal_pilihanganda->id }}">
                                         <input type="hidden" name="soal_pilihanganda_id[]" value="{{ $soal_pilihanganda->id }}">
+                                        <input type="hidden" class="total-nilai" value="{{ $soal_pilihanganda->max_score }}">
+                                        <input type="hidden" class="total-score" value="{{ $soal_pilihanganda->max_score }}">
                                         <td class="text-center">{{ $soal_pilihanganda->urutan }}</td>
                                         <td>
                                             <p>{{ $soal_pilihanganda->question }}</p>
@@ -149,8 +149,12 @@
                                             @if(isset($soal_pilihanganda->options_label) and !empty($soal_pilihanganda->options_label))
                                                 @foreach($soal_pilihanganda->options_label as $key_pil => $pilganda)
 
-                                                    @if($key_pil == $soal_pilihanganda->answer_option)
-                                                        <span class="text-bold">{{ $key_pil }}. {{ $pilganda }}</span>
+                                                    @if($key_pil == $soal_pilihanganda->answer_option and $soal_pilihanganda->answer_option == $soal_pilihanganda->user_answer)
+                                                        <span class="text-green text-bold">{{ $key_pil }}. {{ $pilganda }} <i class="fas fa-check-circle"></i></span>
+                                                    @elseif($key_pil == $soal_pilihanganda->user_answer and $soal_pilihanganda->answer_option != $soal_pilihanganda->user_answer)
+                                                        <span class="text-red text-bold">{{ $key_pil }}. {{ $pilganda }} <i class="fas fa-times-circle"></i></span>
+                                                    @elseif($key_pil == $soal_pilihanganda->answer_option)
+                                                        <span class="text-green text-bold">{{ $key_pil }}. {{ $pilganda }} <i class="fas fa-check-circle"></i></span>
                                                     @else
                                                         {{ $key_pil }}. {{ $pilganda }}
                                                     @endif
@@ -160,13 +164,17 @@
                                                 @endforeach
                                             @endif
                                         </td>
-                                        <td>{{ $soal_pilihanganda->user_answer ? $soal_pilihanganda->options_label[$soal_pilihanganda->user_answer] : '' }}</td>
-                                        <td>{{ $soal_pilihanganda->answer_option ? $soal_pilihanganda->answer_option . '. ' .$soal_pilihanganda->options_label[$soal_pilihanganda->answer_option] : '' }}</td>
-{{--                                        <td class="text-center">{{ $soal_pilihanganda->user_answer }}</td>--}}
-{{--                                        <td class="text-center">{{ $soal_pilihanganda->answer_option }}</td>--}}
                                         <td class="text-center">{{ $soal_pilihanganda->max_score }}</td>
                                     </tr>
                                 @endforeach
+
+                                <tr>
+                                    <td>INFO</td>
+                                    <td colspan="2">
+                                        <span class="text-green text-bold"><i class="fas fa-check-circle"></i></span> = Jawaban Benar |
+                                        <span class="text-red text-bold"><i class="fas fa-times-circle"></i></span> = Jawaban Salah
+                                    </td>
+                                </tr>
                             @endif
                             </tbody>
                         </table>
@@ -187,9 +195,7 @@
                             <thead>
                             <tr class="text-center">
                                 <th width="5%" style="vertical-align: middle;">ID</th>
-                                <th width="25%" style="vertical-align: middle;">Pertanyaan</th>
-                                <th width="20%" style="vertical-align: middle;">Jawaban Asesi</th>
-                                <th width="20%" style="vertical-align: middle;">Jawaban Benar</th>
+                                <th width="65%" style="vertical-align: middle;">Pertanyaan</th>
                                 <th width="5%" style="vertical-align: middle;">Max Score</th>
                                 <th width="10%" style="vertical-align: middle;">Final Score</th>
                                 <th width="15%" style="vertical-align: middle;">Catatan Asesor</th>
@@ -200,23 +206,36 @@
                                 @foreach($soal_essays as $soal_essay)
                                     <tr id="essay-{{ $soal_essay->id }}">
                                         <input type="hidden" name="soal_essay_id[]" value="{{ $soal_essay->id }}">
+                                        <input type="hidden" class="total-score" value="{{ $soal_essay->max_score }}">
                                         <td class="text-center">{{ $soal_essay->urutan }}</td>
-                                        <td>{{ $soal_essay->question }}</td>
-                                        <td>{{ $soal_essay->user_answer }}</td>
-                                        <td>{{ $soal_essay->answer_essay }}</td>
+                                        <td>
+                                            {{ $soal_essay->question }}
+
+                                            <hr />
+
+                                            <p class="text-bold">Jawaban Yang Benar</p>
+                                            {{ $soal_essay->answer_essay }}
+
+                                            <hr />
+
+                                            <p class="text-bold">Jawaban Asesi</p>
+                                            {{ $soal_essay->user_answer }}
+
+                                        </td>
                                         <td class="text-center">{{ $soal_essay->max_score }}</td>
                                         <td class="text-center">
                                             @if(isset($isShow))
+                                                <input type="hidden" class="form-control total-nilai" name="soal_essay[final_score][{{ $soal_essay->id }}]" value="{{ $soal_essay->final_score ?? '' }}">
                                                 {{ $soal_essay->final_score }}
                                             @elseif(isset($isEdit))
-                                                <input type="number" class="form-control" name="soal_essay[final_score][{{ $soal_essay->id }}]" value="{{ $soal_essay->final_score ?? '' }}">
+                                                <input type="number" class="form-control total-nilai" name="soal_essay[final_score][{{ $soal_essay->id }}]" max="{{ $soal_essay->max_score }}" value="{{ old('soal_essay.final_score.' . $soal_essay->id, $soal_essay->final_score) }}">
                                             @endif
                                         </td>
                                         <td>
                                             @if(isset($isShow))
                                                 {{ $soal_essay->catatan_asesor }}
                                             @elseif(isset($isEdit))
-                                                <textarea class="form-control" name="soal_essay[catatan_asesor][{{ $soal_essay->id }}]" rows="3">{{ $soal_essay->catatan_asesor }}</textarea>
+                                                <textarea class="form-control" name="soal_essay[catatan_asesor][{{ $soal_essay->id }}]" rows="3">{{ old('soal_essay.catatan_asesor.' . $soal_essay->id, $soal_essay->catatan_asesor) }}</textarea>
                                             @endif
                                         </td>
                                     </tr>
@@ -230,7 +249,6 @@
         </div>
     </div>
 
-    @if(isset($isShow))
     <div class="form-row">
         <div class="table-responsive mt-2 mb-2">
             <table class="table table-bordered">
@@ -244,27 +262,41 @@
                     <td width="25%">Score Ujian Hasil/Max</td>
                     <td width="2%">:</td>
                     <td class="text-bold">
-                        {{ $total_nilai }} / {{ $total_max }}
-                        @if(!empty($query->final_score_percentage))
-                            {{ _('=') }} {{ $query->final_score_percentage }}{{ _('%') }}
-                        @endif
+                        <span id="total-nilai"></span> {{ _(' / ') }}
+                        <span id="total-score"></span> {{ _(' = ') }}
+                        <span id="final-score-precentage">
+                            @if(isset($isShow))
+                                {{ $query->final_score_precentage }}
+                            @endif
+                        </span>{{ _('%') }}
                     </td>
                 </tr>
                 <tr>
                     <td width="25%">Kompeten</td>
                     <td width="2%">:</td>
                     <td>
-                        @if(!empty($query->is_kompeten))
-                            @if($query->is_kompeten)
-                                <span>Kompeten</span>{{ _(' / ') }}
-                                <span style="text-decoration: line-through;">Tidak Kompeten</span>
+                        @if(isset($isShow))
+                            @if(isset($query->is_kompeten) and in_array($query->is_kompeten, [0, 1]))
+                                @if($query->is_kompeten)
+                                    <span>Kompeten</span>{{ _(' / ') }}
+                                    <span style="text-decoration: line-through;">Tidak Kompeten</span>
+                                @else
+                                    <span style="text-decoration: line-through;">Kompeten</span>{{ _(' / ') }}
+                                    <span>Tidak Kompeten</span>
+                                @endif
                             @else
-                                <span style="text-decoration: line-through;">Kompeten</span>{{ _(' / ') }}
+                                <span>Kompeten</span>{{ _(' / ') }}
                                 <span>Tidak Kompeten</span>
                             @endif
-                        @else
-                            <span>Kompeten</span>{{ _(' / ') }}
-                            <span>Tidak Kompeten</span>
+                        @elseif(isset($isEdit))
+                            <div class="form-check">
+                                <input class="form-check-input" type="radio" name="is_kompeten" value="1" @if(isset($query->is_kompeten) and $query->is_kompeten) {{ _('checked') }} @endif>
+                                <label class="form-check-label">Kompeten</label>
+                            </div>
+                            <div class="form-check">
+                                <input class="form-check-input" type="radio" name="is_kompeten" value="0" @if(isset($query->is_kompeten) and !$query->is_kompeten) {{ _('checked') }} @endif>
+                                <label class="form-check-label">Tidak Kompeten</label>
+                            </div>
                         @endif
                     </td>
                 </tr>
@@ -272,6 +304,50 @@
             </table>
         </div>
     </div>
-    @endif
 
+@endsection
+
+@section('js')
+    <script>
+        function sumTotal(id) {
+            let sum = 0;
+
+            // loop based on css name
+            $(`.${id}`).each(function () {
+
+                // add only if the value is number
+                if (!isNaN(this.value) && this.value.length != 0) {
+                    sum += parseInt(this.value);
+                }
+            });
+
+            // update html
+            $(`#${id}`).html(sum);
+
+            // return value
+            return sum;
+        }
+
+        function percentTotal() {
+            const totalScore = sumTotal('total-score');
+            const totalNilai = sumTotal('total-nilai');
+
+            const percent = Math.ceil( ( parseInt(totalNilai) / parseInt(totalScore) ) * 100);
+            $("#final-score-precentage").html(percent);
+        }
+
+        $(document).ready(function () {
+            // hitung total jika ada perubahan total-score
+            $(".total-score").on('change', function () {
+                percentTotal();
+            });
+            // hitung total jika ada perubahan total-nilai
+            $(".total-nilai").on('change', function () {
+                percentTotal();
+            });
+
+            // run on load
+            percentTotal();
+        });
+    </script>
 @endsection
