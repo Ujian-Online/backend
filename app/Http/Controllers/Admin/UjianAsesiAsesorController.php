3c718ee4ed557adb45c2edc710cf4ca69b6a4777
JuniYadi <juniyadi94@gmail.com>
2021-03-01 16:55:25 +0700

    issue-173: Update Admin UjianAsesiAsesorController - Send Email to Asesor

--- app/Http/Controllers/Admin/UjianAsesiAsesorController.php
+++ app/Http/Controllers/Admin/UjianAsesiAsesorController.php
@@ -4,6 +4,7 @@ namespace App\Http\Controllers\Admin;
 
 use App\DataTables\Admin\UjianAsesiAsesorDataTable;
 use App\Http\Controllers\Controller;
+use App\Mail\AsesorPaketUjianAsesi;
 use App\Order;
 use App\Sertifikasi;
 use App\UjianAsesiAsesor;
@@ -17,6 +18,7 @@ use Illuminate\Http\JsonResponse;
 use Illuminate\Http\RedirectResponse;
 use Illuminate\Http\Request;
 use Illuminate\Http\Response;
+use Illuminate\Support\Facades\Mail;
 use Illuminate\View\View;
 
 class UjianAsesiAsesorController extends Controller
@@ -183,6 +185,18 @@ class UjianAsesiAsesorController extends Controller
                 'asesor_id',
                 'ujian_jadwal_id',
             ]);
+
+            // update data
+            $query->update($dataInput);
+
+            // Notification to Asesor
+            if(!empty($dataInput['asesor_id'])) {
+                // get asesor detail
+                $userAsesor = User::where('type', 'assesor')->where('id', $dataInput['asesor_id'])->firstOrFail();
+
+                // Kirim Email ke Asesor
+                Mail::to($userAsesor->email)->send(new AsesorPaketUjianAsesi($id));
+            }
         } elseif($user->can('isAsesor')) {
             // validate input
             $request->validate([
@@ -197,13 +211,11 @@ class UjianAsesiAsesorController extends Controller
                 'is_kompeten',
                 'final_score_percentage',
             ]);
+
+            // update data
+            $query->update($dataInput);
         }
 
-
-
-
-        // update data
-        $query->update($dataInput);
 
         // redirect
         return redirect()
