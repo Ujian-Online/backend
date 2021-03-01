62ac7298491977211c83ceffc233888e388e7043
JuniYadi <juniyadi94@gmail.com>
2021-03-01 20:00:01 +0700

    issue-176: Update API Route - PemegangSertifikasi

--- routes/api.php
+++ routes/api.php
@@ -32,6 +32,7 @@ Route::name('api.')
         // List Sertifikasi
         Route::get('sertifikasi', 'Api\SertifikasiController@index')->name('sertifikasi');
         Route::get('sertifikasi/{id}', 'Api\SertifikasiController@show')->name('sertifikasi.show');
+        Route::get('pemegang-sertifikasi', 'Api\SertifikasiController@pemegangSertifikasi')->name('sertifikasi.pemegangSertifikasi');
 
         Route::middleware(['auth:api'])
             ->group(function () {
