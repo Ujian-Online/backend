a6d8cfaea6443e50cb1c50b8bf65da7be68d2624
JuniYadi <juniyadi94@gmail.com>
2021-03-01 19:59:54 +0700

    issue-176: Update API SertifikasiController - PemegangSertifikasi

--- app/Http/Controllers/Api/SertifikasiController.php
+++ app/Http/Controllers/Api/SertifikasiController.php
@@ -3,6 +3,7 @@
 namespace App\Http\Controllers\Api;
 
 use App\Http\Controllers\Controller;
+use App\UjianAsesiAsesor;
 use Illuminate\Database\Eloquent\Builder;
 use Illuminate\Database\Eloquent\Collection;
 use Illuminate\Database\Eloquent\Model;
@@ -81,5 +82,37 @@ class SertifikasiController extends Controller
             ->where('id', $id)
             ->where('is_active', 1)
             ->firstOrFail();
+    }
+
+    /**
+     * Pemegang Sertifikasi Index DataTables
+     *
+     * @param Request $request
+     * @return JsonResponse
+     *
+     * * @OA\Get(
+     *   path="/api/pemegang-sertifikasi",
+     *   tags={"Sertifikasi"},
+     *   summary="Pemegang Sertifikasi Lists",
+     *
+     *   @OA\Response(
+     *      response="200",
+     *      description="OK"
+     *   )
+     * )
+     */
+    public function pemegangSertifikasi(Request $request)
+    {
+        $query = UjianAsesiAsesor::select([
+            'ujian_asesi_asesors.id as id',
+            'user_asesis.name as name',
+            'sertifikasis.title as sertifikasi'
+        ])
+            ->leftJoin('users', 'users.id', '=', 'ujian_asesi_asesors.asesi_id')
+            ->leftJoin('user_asesis', 'user_asesis.user_id', '=', 'users.id')
+            ->leftJoin('sertifikasis', 'sertifikasis.id', '=', 'ujian_asesi_asesors.sertifikasi_id');
+
+        // return query datatables
+        return datatables()->eloquent($query)->toJson();
     }
 }
