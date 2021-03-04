58311684d8b95c8b7f520e82669c374385619daf
JuniYadi <juniyadi94@gmail.com>
2021-03-04 08:44:57 +0700

    [HOT-FIX] Update API PemegangSerifikasiController - Only Show With Status Selesai

--- app/Http/Controllers/Api/PemegangSerifikasiController.php
+++ app/Http/Controllers/Api/PemegangSerifikasiController.php
@@ -37,7 +37,7 @@ class PemegangSerifikasiController extends Controller
             ->join('user_asesis', 'user_asesis.user_id', '=', 'ujian_asesi_asesors.asesi_id')
             ->join('sertifikasis', 'sertifikasis.id', '=', 'ujian_asesi_asesors.sertifikasi_id')
             ->whereNotNull('ujian_asesi_asesors.ujian_start')
-            ->where('status', 'selesai');
+            ->where('ujian_asesi_asesors.status', 'selesai');
 
         return datatables()
             ->eloquent($query)
