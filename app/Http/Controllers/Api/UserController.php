83c0015875ad0b475d3a9df7c68d87e4a85b5cf3
JuniYadi <juniyadi94@gmail.com>
2021-03-08 19:38:57 +0700

    issue-36: Update API Upload Profile atau Paraf

--- app/Http/Controllers/Api/UserController.php
+++ app/Http/Controllers/Api/UserController.php
@@ -6,6 +6,7 @@ use App\User;
 use Illuminate\Http\Request;
 use App\Http\Controllers\Controller;
 use Illuminate\Support\Facades\Auth;
+use Illuminate\Support\Facades\Hash;
 use Illuminate\Support\Facades\Validator;
 
 class UserController extends Controller
@@ -52,5 +53,78 @@ class UserController extends Controller
 
         // return json response
         return response()->json($response, $response['code']);
+    }
+
+    /**
+     * @param Request $request
+     * @return \Illuminate\Http\JsonResponse
+     *
+     *
+     * @OA\Post(
+     *   path="/api/user",
+     *   tags={"User"},
+     *   summary="Update Foto atau TTD",
+     *   security={{"passport":{}}},
+     *
+     *     @OA\RequestBody(
+     *         required=true,
+     *         description="Update Foto atau TTD",
+     *         @OA\MediaType(
+     *             mediaType="multipart/form-data",
+     *             @OA\Schema(
+     *                 @OA\Property(
+     *                     property="upload_profile",
+     *                     description="Upload Gambar Profile. Ekstensi: JPG/JPEG/PNG",
+     *                     type="file",
+     *                     example="profile.png"
+     *                 ),
+     *                 @OA\Property(
+     *                     property="upload_sign",
+     *                     description="Upload TTD/Paraf. Ekstensi: JPG/JPEG/PNG",
+     *                     type="file",
+     *                     example="paraf.png"
+     *                 ),
+     *             ),
+     *         ),
+     *     ),
+     *     @OA\Response(
+     *         response="200",
+     *         description="OK"
+     *     )
+     * )
+     */
+    public function signature(Request $request)
+    {
+        // validate input
+        $request->validate([
+            'upload_profile'    => 'nullable|mimes:jpg,jpeg,png',
+            'upload_sign'       => 'nullable|mimes:jpg,jpeg,png'
+        ]);
+
+        // get user login
+        $user = $request->user();
+
+        // find by id and update
+        $query = User::findOrFail($user->id);
+
+        // upload file profile picture to s3
+        if($request->file('upload_profile')) {
+            $query->media_url = upload_to_s3($request->file('upload_profile'));
+        }
+
+        // upload file ttd/paraf to s3
+        if($request->file('upload_sign')) {
+            $query->media_url_sign_user = upload_to_s3($request->file('upload_sign'));
+        }
+
+        // update data
+        $query->update();
+
+        // update response
+        return response()->json([
+            'code' => 200,
+            'message' => 'success',
+            'data' => $query,
+        ]);
     }
 }
