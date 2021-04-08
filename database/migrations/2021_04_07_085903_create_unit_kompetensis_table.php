fd81d633833c84f7ff9a1ea0a07238e5cf56a128
JuniYadi <juniyadi94@gmail.com>
2021-04-08 16:48:01 +0700

    [update] Rename Sertifikasi Unit Kompetensi Menjadi Unit Kompetensi

---
+++ database/migrations/2021_04_07_085903_create_unit_kompetensis_table.php
@@ -0,0 +1,34 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateUnitKompetensisTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('unit_kompetensis', function (Blueprint $table) {
+            $table->bigIncrements('id');
+            $table->string('order');
+            $table->bigInteger('sertifikasi_id');
+            $table->bigInteger('unit_kompetensi_id');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('unit_kompetensis');
+    }
+}
