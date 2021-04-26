48c3e6e319cf2f6fb4c673e291a7adb34ccf2a17
JuniYadi <juniyadi94@gmail.com>
2021-04-26 22:26:07 +0700

    Update APL02 View and Print (Footer)

--- resources/views/admin/assesi/apl02-print.blade.php
+++ resources/views/admin/assesi/apl02-print.blade.php
@@ -121,10 +121,10 @@
                                         <td>
                                             <p>{!! nl2br($ukelement->desc) !!}</p>
                                         </td>
-                                        <td style="vertical-align: middle;">
+                                        <td class="text-center" style="vertical-align: middle;">
                                             <input type="checkbox" @if($ukelement->is_verified) {{__('checked')}} @endif onclick="return false;" />
                                         </td>
-                                        <td style="vertical-align: middle;">
+                                        <td class="text-center" style="vertical-align: middle;">
                                             <input type="checkbox" @if(!$ukelement->is_verified) {{__('checked')}} @endif onclick="return false;" />
                                         </td>
                                         <td>
@@ -138,7 +138,60 @@
                 </div>
             @endforeach
         @endif
-    </div>
+
+        <div class="form-group col-md-12">
+            <div class="table-responsive">
+                <table class="table table-bordered">
+                    <tbody>
+                        <tr>
+                            <td>
+                                <p>Nama Asesi:</p>
+                                <p>{{ (isset($user) and !empty($user->asesi)) ? $user->asesi->name : '' }}</p>
+                            </td>
+                            <td>
+                                <p>Tanggal:</p>
+                                <p>{{ now()->format('d-m-Y') }}</p>
+                            </td>
+                            <td>
+                                <p>Tanda Tangan Asesi:</p>
+                                <p>
+                                    @if(isset($user) and !empty($user->media_url_sign_user))
+                                        <img height="70px" width="70px" src="{{ (isset($user) and !empty($user->media_url_sign_user)) ? $user->media_url_sign_user : '' }}">
+                                    @else
+                                        {{ __('-') }}
+                                    @endif
+                                </p>
+                            </td>
+                        </tr>
+                        <tr class="bg-orange text-bold">
+                            <td colspan="3">Ditinjau oleh Asesor:</td>
+                        </tr>
+                        <tr>
+                            <td>
+                                <p>Nama Asesor:</p>
+                                <p>{{ (isset($ujianasesiasesor) and !empty($ujianasesiasesor->userasesor) and !empty($ujianasesiasesor->userasesor->asesor)) ? $ujianasesiasesor->userasesor->asesor->name : '' }}</p>
+                            </td>
+                            <td>
+                                <p>Rekomendasi:</p>
+                                <p>{{ __('Asesmen dapat dilanjutkan/ tidak dapat dilanjutkan') }}</p>
+                            </td>
+                            <td>
+                                <p>Tanda Tangan dan Tanggal:</p>
+                                <p>{{ now()->format('d-m-Y') }}</p>
+                                <p>
+                                    @if(isset($ujianasesiasesor) and !empty($ujianasesiasesor->userasesor) and !empty($ujianasesiasesor->userasesor->media_url_sign_user))
+                                        <img height="70px" width="70px" src="{{ (isset($ujianasesiasesor) and !empty($ujianasesiasesor->userasesor)) ? $ujianasesiasesor->userasesor->media_url_sign_user : '' }}">
+                                    @else
+                                        {{ __('-') }}
+                                    @endif
+                                </p>
+                            </td>
+                        </tr>
+                    </tbody>
+                </table>
+                <p>Diadaptasi dari template yang disediakan di Departemen Pendidikan dan Pelatihan, Australia. Merancang instrumen asesmen dalam VET. 2008</p>
+            </div>
+        </div>
 @endsection
 
 @section('js')
