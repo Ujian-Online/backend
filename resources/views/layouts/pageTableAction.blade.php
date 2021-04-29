e3dea1162f093dd01de0b7828ff898e11ffded0c
JuniYadi <juniyadi94@gmail.com>
2021-04-29 14:10:56 +0700

    Update Table Button Print

--- resources/views/layouts/pageTableAction.blade.php
+++ resources/views/layouts/pageTableAction.blade.php
@@ -2,7 +2,7 @@
 <a href="{{ $url_sertifikasi_uk }}" class="btn btn-info btn-sm mb-2" title="Buka Unit Kompetensi dari Sertifikasi: {{ $title }}"><i class="fas fa-list-ul"></i></a>
 @endif
 @if(isset($url_print))
-<a href="#" onclick="window.open('{{ $url_print }}', '', 'fullscreen=yes');" class="btn btn-warning btn-sm mb-2" title="Print Dokumen: {{ $title }}"><i class="fas fa-print"></i></a>
+<button class="btn btn-warning btn-sm mb-2" onclick="window.open('{{ $url_print }}', '', 'fullscreen=yes');" title="Print Dokumen: {{ $title }}"><i class="fas fa-print"></i></button>
 @endif
 @if(isset($url_show))
 <a href="{{ $url_show }}" class="btn btn-primary btn-sm mb-2 btn-show" title="Detail: {{ $title }}"><i class="far fa-eye"></i></a>
