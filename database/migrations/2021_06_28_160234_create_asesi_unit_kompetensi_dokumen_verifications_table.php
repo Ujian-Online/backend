a3e597c7cc709049383ed4b89bd0481c198936fa
JuniYadi <juniyadi94@gmail.com>
2021-06-28 17:07:50 +0700

    [HOT-FIX] Added Migration AsesiUnitKompetensiDokumenVerification

---
+++ database/migrations/2021_06_28_160234_create_asesi_unit_kompetensi_dokumen_verifications_table.php
@@ -0,0 +1,37 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateAsesiUnitKompetensiDokumenVerificationsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('asesi_unit_kompetensi_dokumen_verifications', function (Blueprint $table) {
+            $table->bigIncrements('id');
+            $table->bigInteger('asesi_id');
+            $table->bigInteger('asesor_id')->nullable();
+            $table->text('recommendation')->nullable();
+            $table->boolean('jenis_pengalaman')->default(false);
+            $table->date('asesi_verification_date')->nullable();
+            $table->date('asesor_verification_date')->nullable();
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('asesi_unit_kompetensi_dokumen_verifications');
+    }
+}
